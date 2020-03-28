87c2eae8a69482361198f48266aceb2a6134d3b8
Gil Solis <gilsolisuw@gmail.com>
2020-03-28 11:01:40 -0700

    styling

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.set("view engine", "handlebars");
 app.use(require("./routes"));
 
 
-db.sequelize.sync({}).then(function () {
-  app.listen(PORT, function () {
-    console.log("Listening on PORT", PORT);
-  });
+db.sequelize.sync({}).then(function() {
+    app.listen(PORT, function() {
+        console.log("Listening on PORT", PORT);
+    });
 });
-
